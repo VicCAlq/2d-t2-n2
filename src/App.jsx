e1e1d740eff7e59fa14bb557dbec9a54ed554634
Pedro-Carvalho-Nave <pedro.benicio@nave.org.br>
2026-08-07 16:39:12 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AdicionarRSS from "./components/AdicionarRSS";
 import Exemplo from "./components/Exemplo";
 import { useState } from "react";
+import TabelaDeNoticias from "./components/TabelaDeNoticias";
 
 const styles = {
   container: {
@@ -17,12 +18,16 @@ const styles = {
 export default function App() {
   
   const [feed, setFeed] = useState()
+  const [tabelaExibida, setTabelaExibida] = useState()
+
+
 
   return (
     <div style={styles.container}>
       <h1>Crie seu aplicativo React aqui</h1>
-      <Exemplo>Componente de exemplo aqui</Exemplo>
+      <Exemplo setFeed={setFeed}>Componente de exemplo aqui</Exemplo>
       <AdicionarRSS setFeed={setFeed}/>
+      <TabelaDeNoticias feed={feed}/>
     </div>
   );
 }
