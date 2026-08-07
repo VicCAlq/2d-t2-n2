0dd082efabccd72ab88c554f823bff924c8fdcb8
David-luduvico-nave <david.luduvico@nave.org.br>
2026-08-07 16:35:25 -0300

    Arquivo classes.js criado com as duas classes da pagina

---
+++ src/classes.js
@@ -0,0 +1,24 @@
+class Fonte {
+    constructor(nome, endereco, descricao, categoria)
+
+    {
+        this.nome = nome
+        this.endereco = endereco
+        this.descricao = descricao
+        this.categoria = categoria
+    }
+   
+}
+
+class Noticia {
+    constructor(nome, endereco, descricao, dataDePublicacao, categoria)
+
+    {
+        this.nome = nome
+        this.endereco = endereco
+        this.descricao = descricao
+        this.dataDePublicacao = dataDePublicacao
+        this.categoria = categoria
+    }
+
+}
