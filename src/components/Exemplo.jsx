f7415965a38e637ebcc3c7b4d8b7a69e38414353
Pedro-Carvalho-Nave <pedro.benicio@nave.org.br>
2026-08-07 16:40:22 -0300

    Update Exemplo.jsx

--- src/components/Exemplo.jsx
+++ src/components/Exemplo.jsx
@@ -1,5 +1,21 @@
-export default function Exemplo({ children }) {
-  return(<>
-    <h2>{children}</h2>
-  </>)
+import { useState } from "react";
+
+
+export default function TabelaDeNoticias({feed}) {
+ 
+    if (!feed) {
+        return(<></>)
+    } else {
+        return(
+            <div>
+            <p>
+                Carregue a lista de noticias abaixo:
+            </p>
+            <div>
+            //resultado 
+            </div>
+            </div>
+        )
+    }
+
 }
