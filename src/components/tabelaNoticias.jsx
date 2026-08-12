4aef949d14eb194c23b9aea392231ca361053cc4
David-luduvico-nave <david.luduvico@nave.org.br>
2026-08-12 14:02:54 -0300

    TabelaCriada

---
+++ src/components/tabelaNoticias.jsx
@@ -0,0 +1,34 @@
+function TabelaNoticias() {
+
+    return(
+        <table>
+
+            <thead>
+                <tr>
+                    <th>Matéria</th>
+                    <th>Fonte</th>
+                    <th>Categoria(s)</th>
+                    <th>Link</th>
+                    <th>Descrição</th>
+                    <th>Data de Publicação</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {noticias.map((noticia) => (
+                    <tr>
+                        <td>{noticia.nome}</td>
+                        <td>{noticia.fonte}</td>
+                        <td>{noticia.categoria}</td>
+                        <td>{noticia.endereco}</td>
+                        <td>{noticia.descricao}</td>
+                        <td>{noticia.dataDePublicacao}</td>
+                    </tr>
+                ))}
+            </tbody>
+            
+        </table>
+    )
+}
+
+export default TabelaNoticias
