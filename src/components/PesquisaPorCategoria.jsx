1b04a8886f6bfc1324181639be7582b3517f74b4
Pedro-Carvalho-Nave <pedro.benicio@nave.org.br>
2026-08-18 11:56:14 -0300

    Refactor category filtering in PesquisaPorCategoria
    ASF

--- src/components/PesquisaPorCategoria.jsx
+++ src/components/PesquisaPorCategoria.jsx
@@ -1,53 +1,43 @@
-import { useState } from "react";
 import { filtrarNoticiasPorCategoria } from './database.js';
 
-function PesquisaPorCategoria({ noticias, aoFiltrar , todasNoticias }) {
-    const [termo, setTermo] = useState("");
+function PesquisaPorCategoria({ noticias, aoFiltrar, todasNoticias }) {
 
-    
+    const listaCategoriasTratadas = todasNoticias.map((noticia) => {
+        if (!noticia.categorias || noticia.categorias.length === 0) {
+            return 'sem categoria';
+        }
+        
+        return Array.isArray(noticia.categorias) 
+            ? noticia.categorias.join(', ') 
+            : noticia.categorias;
+    });
 
-    return(<>
-        <select
-          defaultValue=""
-          onChange={async (e) => {
-            const valor = e.target.value;
-            if (!valor) {
-              aoFiltrar(noticias);
-              return;
-            }
-            const resultado = await filtrarNoticiasPorCategoria(valor);
-            aoFiltrar(resultado);
-          }}
-        >
-          <option value="" disabled>Selecione uma Categoria</option>
-          {todasNoticias.map((noticia, index) => {
-            let categoria = 'sem categoria';
-            if (noticia.categorias && noticia.categorias.length) {
-                categoria = noticia.categorias;
-            }
-            
-            let valor = categoria;
-            if (Array.isArray(categoria)) {
-                valor = categoria.join(', ');
-            }
-            
-            if (!valor || valor === 'sem categoria') {
-                return (
-                  
-                    <option key={`sem-categoria-${index}`} value="sem categoria">
-                  sem categoria
+    const categoriasUnicas = [...new Set(listaCategoriasTratadas)];
+
+    return (
+        <>
+            <select
+              defaultValue=""
+              onChange={async (e) => {
+                const valor = e.target.value;
+                if (!valor) {
+                  aoFiltrar(noticias);
+                  return;
+                }
+                const resultado = await filtrarNoticiasPorCategoria(valor);
+                aoFiltrar(resultado);
+              }}
+            >
+              <option value="" disabled>Selecione uma Categoria</option>
+              
+              {categoriasUnicas.map((categoria, index) => (
+                <option key={`${categoria}-${index}`} value={categoria}>
+                  {categoria}
                 </option>
-              );
-            } else {
-              return (
-                <option key={`${valor}-${index}`} value={valor}>
-                  {valor}
-                </option>
-              );
-            }
-          })}
-        </select>
-    </>)
+              ))}
+            </select>
+        </>
+    );
 }
 
 export default PesquisaPorCategoria;
