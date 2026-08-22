281c2bf21fdbc1b3d29f819d0caccc06a02d3396
joao-gomes-nave <joao.gomes@nave.org.br>
2026-08-22 15:53:03 -0300

    correção

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import FormularioFontes from "./components/FormularioFontes";
+import FormularioFonte from "./components/FormularioFonte";
 
 const styles = {
   container: {
@@ -25,7 +25,7 @@ export default function App() {
     <div style={styles.container}>
       <h1 style={styles.title}>Agregador de Notícias</h1>
 
-      <FormularioFontes onAdicionarFonte={handleAdicionarFonte} />
+      <FormularioFonte onAdicionarFonte={handleAdicionarFonte} />
 
       <h2>Fontes cadastradas ({fontes.length})</h2>
       <ul>
